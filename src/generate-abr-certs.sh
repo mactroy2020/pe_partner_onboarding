d0003bc7c51d6d1bbd5cc31a4c27baa7bbfb43e0
Troy Cochcran <mactroy@apple.com>
2022-12-07 18:40:09 -0500

    fix: generate complete ssl conf from start

--- src/generate-abr-certs.sh
+++ src/generate-abr-certs.sh
@@ -2,14 +2,14 @@
 
 echo "Generating Encryption Keys"
 openssl ecparam -name prime256v1 -genkey -noout -out /certs/encryption-key.pem
-openssl req -config /ca/config/openssl.cfg  -new -key /certs/encryption-key.pem -out /certs/encryption.csr
+openssl req -config /ca/config/openssl.cnf  -new -key /certs/encryption-key.pem -out /certs/encryption.csr
 
 echo "Generating Server Keys"
 openssl genrsa -out /certs/server-key.pem 4096
-openssl req -new -config /ca/config/openssl.cfg -key /certs/server-key.pem -out /certs/server.csr
+openssl req -new -config /ca/config/openssl.cnf -key /certs/server-key.pem -out /certs/server.csr
 
 echo "Generating Client Keys"
 openssl genrsa -out /certs/client-key.pem 2048
-openssl req -new -config /ca/config/openssl.cfg -key /certs/client-key.pem -out /certs/client.csr
+openssl req -new -config /ca/config/openssl.cnf -key /certs/client-key.pem -out /certs/client.csr
 
 echo "Done"
